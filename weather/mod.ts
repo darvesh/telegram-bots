c9ba67fada267723d835a11055b328d809075bee
Darvesh <hello@darve.sh>
2026-09-02 03:55:05 +0530

    Switch to npm:grammy for webhookCallback import

--- weather/mod.ts
+++ weather/mod.ts
@@ -1,12 +1,12 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { webhookCallback } from "https://deno.land/x/grammy@v1.14.1/mod.ts";
+
+import { webhookCallback } from "npm:grammy";
 
 import { bot } from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 const SECRET = Deno.env.get("SECRET");
 
-serve(async (req) => {
+Deno.serve(async (req) => {
 	if (req.method === "POST") {
 		const url = new URL(req.url);
 		if (url.pathname.slice(1) === SECRET) {
